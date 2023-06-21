641ee37d57e74ead33c0c86fb899adbd012e59e5
altynovbayel <altynovich30@gmail.com>
2023-06-21 21:10:13 +0600

    slider

--- src/components/stocks/slide/index.jsx
+++ src/components/stocks/slide/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import c from './slide.module.scss'
 
-const Slide = ({img}) => {
+const Slide = ({img, title, backgroundColor, color}) => {
   return (
-    <div className={c.slide}>
-      <img 
-        src={img} 
-        alt="banner"
-      />
+    <div
+      className={c.slide}
+      style={{backgroundColor: backgroundColor, color: color }}
+    >
+      <div className={c.slide_text}>
+        <h2>{title}</h2>
+        <button style={{backgroundColor: color}}>
+          Узнайте о наших продуктах
+        </button>
+      </div>
+      <div className={c.slider_img}>
+        <img src={img} alt="" />
+      </div>
     </div>
   )
 }
